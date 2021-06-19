748f174132ed5e991d242fa569305bcb6028a8a9
Oleg Kostyaev <kostyaev@gmail.com>
2021-06-19 10:52:31 +0300

    update

--- src/com/javabasic/work53/Task53.java
+++ src/com/javabasic/work53/Task53.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Task53 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter organisation name --> ");
+        System.out.print("Enter a few word --> ");
         String strIn = sc.nextLine();
         String lastWord = strIn.substring(strIn.lastIndexOf(" ")+1);
         String firstWord = strIn.substring(0,strIn.indexOf(" "));
